46b67d80705c3428aac3b70e6ba607e5a593aefb
beatface <emma.beaton@gmail.com>
2016-04-14 15:17:41 -0500

    formatted list output

--- test.rb
+++ test.rb
@@ -1,17 +1,28 @@
 
 def create_grocery_list
     print "What should this list be called? "
-    return {"list_name"=>gets.chomp, "items"=>[]}
+    return { "list_name" => gets.chomp, "items" => [] }
 end
 
 def add_item
     print "What would you like to add? "
     item_name = gets.chomp
-    return {"name"=>item_name}
+    print "How much? "
+    quantity = gets.chomp
+    return { "name" => item_name, "quantity" => quantity }
+end
+
+def print_list(list)
+    puts "List: #{list['name']}"
+    puts "----"
+    list['items'].each do |item|
+        puts "Item: #{item['name']}"
+        puts "Quantity: #{item['quantity']}"
+        puts "--"
+    end
 end
 
 list = create_grocery_list
-puts list.inspect
 new_item = add_item
 list["items"] << new_item
-puts list
+print_list(list)
