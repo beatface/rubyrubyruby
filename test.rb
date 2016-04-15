cc318dd2cd31510cddc036cf0bfae77f967496f9
beatface <emma.beaton@gmail.com>
2016-04-15 11:32:37 -0500

    formatting update

--- test.rb
+++ test.rb
@@ -5,24 +5,34 @@ def create_grocery_list
 end
 
 def add_item
-    print "What would you like to add? "
+    print "What would you like to add? (type 'n' to stop)"
     item_name = gets.chomp
-    print "How much? "
-    quantity = gets.chomp
-    return { "name" => item_name, "quantity" => quantity }
+    if item_name.downcase == 'n'
+        return 'n'
+    else
+        print "How much? "
+        quantity = gets.chomp
+        return { "name" => item_name, "quantity" => quantity }
+    end
 end
 
 def print_list(list)
     puts "List: #{list['name']}"
     puts "----"
     list['items'].each do |item|
-        puts "Item: #{item['name']}"
-        puts "Quantity: #{item['quantity']}"
+        puts "\s\sItem: #{item['name']}"
+        puts "\s\sQuantity: #{item['quantity']}"
         puts "--"
     end
 end
 
 list = create_grocery_list
-new_item = add_item
-list["items"] << new_item
-print_list(list)
+loop do
+    new_item = add_item
+    if new_item == 'n'
+        break
+    else
+        list["items"] << new_item
+        print_list(list)
+    end
+end
